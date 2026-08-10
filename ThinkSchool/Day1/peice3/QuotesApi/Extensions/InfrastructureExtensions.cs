2d59df1042a5b2ce6542112566c91788164af7b7
jeet1703 <prabhjeetdec03@gmail.com>
2026-08-10 15:35:56 +0530

    Day 1 task 3

---
+++ ThinkSchool/Day1/peice3/QuotesApi/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using QuotesApi.Data;
+using QuotesApi.Repositories;
+
+namespace QuotesApi.Extensions;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString =
+            configuration.GetConnectionString("QuotesDb")
+            ?? "Data Source=quotes.db";
+
+        services.AddDbContext<QuotesDbContext>(options =>
+            options.UseSqlite(connectionString));
+
+        services.AddScoped<IQuoteRepository, QuoteRepository>();
+
+        return services;
+    }
+
+    public static async Task ApplyMigrationsAsync(
+        this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var db = scope.ServiceProvider
+            .GetRequiredService<QuotesDbContext>();
+
+        await db.Database.MigrateAsync();
+    }
+}
